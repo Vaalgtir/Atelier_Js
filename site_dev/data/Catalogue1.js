6986b6f055a3a7280d8824bdf89c669669527419
Vaalgtir <e.eniona2@gmail.com>
2020-11-09 17:10:49 +0100

    ajout dev Eric

---
+++ site_dev/data/Catalogue1.js
@@ -0,0 +1,105 @@
+var catalog1 = [{
+    "name": "Grosnounours",
+    "description": "un nounours géant",
+    "image": "data/images/grosnounours.jpg",
+    "price": "50",
+    "Id": "10"
+},
+{
+    "name": "Nounours",
+    "description": "un joli nounours un peu étrange",
+    "image": "data/images/nounourschelou.jpg",
+    "price": "35",
+    "Id": "11"
+},
+{
+    "name": "Mouton",
+    "description": "un petit mouton",
+    "image": "data/images/mouton.jpg",
+    "price": "40",
+    "Id": "12"
+},
+{
+    "name": "NounoursCamo ",
+    "description": "Vous ne l'aviez pas vu arriver",
+    "image": "data/images/nounourscamo.jpg",
+    "price": "35",
+    "Id": "13"
+},
+{
+    "name": "NounoursCovid ",
+    "description": "Tellement 2020",
+    "image": "data/images/nounourscovid.jpg",
+    "price": "35",
+    "Id": "14"
+},
+{
+    "name": "NounoursStValentin ",
+    "description": "Bouquet et chocolat non fournis",
+    "image": "data/images/nounoursentrepenant.jpg",
+    "price": "35",
+    "Id": "15"
+},
+{
+    "name": "NounoursForrest ",
+    "description": "Sauf que lui il ne court pas",
+    "image": "data/images/nounoursforrest.jpg",
+    "price": "35",
+    "Id": "16"
+},
+{
+    "name": "NounoursAbime ",
+    "description": "Une ébauche de momie à personnaliser",
+    "image": "data/images/nounoursmaltraite.jpg",
+    "price": "35",
+    "Id": "17"
+},
+{
+    "name": "NounoursNaruto ",
+    "description": "Lui aussi médite tout seul sur un banc",
+    "image": "data/images/nounoursnaruto.jpg",
+    "price": "37",
+    "Id": "18"
+},
+{
+    "name": "NounoursNeige ",
+    "description": "Tu ne sais rien Jean Neige!",
+    "image": "data/images/nounoursneige.jpg",
+    "price": "35",
+    "Id": "19"
+},
+{
+    "name": "Nounoursoublie ",
+    "description": "Un peu dépressif mais attachant",
+    "image": "data/images/nounoursoublie.jpg",
+    "price": "30",
+    "Id": "110"
+},
+{
+    "name": "NounoursRando ",
+    "description": "S'entend très bien avec le NounoursCamo!",
+    "image": "data/images/nounoursrando.jpg",
+    "price": "25",
+    "Id": "111"
+},
+{
+    "name": "NounoursVigie ",
+    "description": "Pour surveiller les enfants",
+    "image": "data/images/nounoursvigie.jpg",
+    "price": "35",
+    "Id": "112"
+},
+{
+    "name": "FullofNounours ",
+    "description": "Pour une overdose de nounours",
+    "image": "data/images/overdosedenounours.jpg",
+    "price": "60",
+    "Id": "113"
+},
+{
+    "name": "PanthereRose ",
+    "description": "Ne la jugez pas, la célébrité est un fardeau lourd à porter",
+    "image": "data/images/pantherealcoolique.jpg",
+    "price": "20",
+    "Id": "114"
+}]
