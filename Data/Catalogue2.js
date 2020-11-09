4eeb1952b0e667fc3739402b0bb9358d1448a182
MarionJS <74191216+MarionJS@users.noreply.github.com>
2020-11-09 16:58:35 +0100

    Add files via upload

--- Data/Catalogue2.js
+++ Data/Catalogue2.js
@@ -3,60 +3,60 @@
     "description": "Une petite poupee classique",
     "image": "images/poupee.jpg",
     "price": "30",
-    "Id": "21"
+    "Id": "20"
 },
 {
     "name": "PantinBois",
     "description": "Des pantins en bois",
     "image": "images/poupeebois.jpg",
     "price": "25",
-    "Id": "22"
+    "Id": "21"
 }, {
     "name": "CasseNoisette",
     "description": "Pas le ballet.",
     "image": "images/poupeecassenoisette.jpg",
     "price": "40",
-    "Id": "23"
+    "Id": "22"
 }, {
     "name": "Poupeebll",
     "description": "Je vous jure qu'elle a bougé!",
     "image": "images/poupeechelou.jpg",
     "price": "10",
-    "Id": "24"
+    "Id": "23"
 }, {
     "name": "Clown",
     "description": "Clown dépressif",
     "image": "images/poupeeclown.jpg",
     "price": "20",
-    "Id": "25"
+    "Id": "24"
 }, {
     "name": "Nain",
     "description": "Rigole en courant dans l'herbe",
     "image": "images/poupeenain.jpg",
     "price": "30",
-    "Id": "26"
+    "Id": "25"
 }, {
     "name": "PetitePoupee",
     "description": "Moitié prix",
     "image": "images/poupeepetite.jpg",
     "price": "15",
-    "Id": "27"
+    "Id": "26"
 }, {
     "name": "PoupeeCharmante",
     "description": "Une charmante poupée dont je vous saurez gré de me débarasser.",
     "image": "images/poupeepsycho.jpg",
     "price": "2",
-    "Id": "28"
+    "Id": "27"
 }, {
     "name": "PoupeeTricot",
     "description": "Amigurumi",
     "image": "images/poupeetricot.jpg",
     "price": "20",
-    "Id": "29"
+    "Id": "28"
 }, {
     "name": "Troll",
     "description": "A ne pas laisser devant un chat.",
     "image": "images/poupeetroll.jpg",
     "price": "35",
-    "Id": "210"
+    "Id": "29"
 }]
